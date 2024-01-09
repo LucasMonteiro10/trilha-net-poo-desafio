5c2c57717f4dcba66da7cbcd85b19393692bbef3
LucasMonteiro10 <lmvb@discente.ifpe.edu.br>
2024-01-09 18:34:09 -0300

    Implementa método InstalarAplicativo(), adiciona propriedades e cria testes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia n1 = new Nokia("99696-9696", "C2", "123456", 32);
+n1.Ligar();
+n1.InstalarAplicativo("Whatsapp");
+
+Iphone i1 = new Iphone("99898-9898", "14 Pro Max", "654321", 128);
+i1.ReceberLigacao();
+i1.InstalarAplicativo("Telegram");
